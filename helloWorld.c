48badffe0a1f4a5a404c2440745033e05a0879cc
UnkeptBedroom <wilfredo.martinez52@outlook.com>
2025-09-28 09:21:31 -0500

    Hello World C Programs

---
+++ helloWorld.c
@@ -0,0 +1,20 @@
+/*
+    * Program: Hello World
+    * Created by: Wilfredo Martinez
+    * Created: 9/28/2025
+    * Last Modified: 9/28/2025
+*/
+
+/*Run this program by opening a bash session and doing the following:
+  cc helloWorld.c
+  ./a.out
+*/
+
+
+#include <stdio.h>
+
+int main()
+    {
+        /*This command prints Hello World to the terminal.*/
+        printf("Hello, world!\n");
+    }
